839a6cbb9bdb188170aed73ab59b0415c6fb63dc
Basix <basix@basix.tech>
2022-02-10 05:33:42 +0900

    Allow forwarded refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
 import React, { HTMLAttributes, useCallback, useRef, ReactPortal } from "react";
 import { createPortal } from "react-dom";
+import mergeRefs from "react-merge-refs";
 
-export default function Template({
+export default React.forwardRef<HTMLTemplateElement>(function Template({
   children,
   ...props
-}: HTMLAttributes<HTMLTemplateElement>) {
+}: HTMLAttributes<HTMLTemplateElement>, ref) {
   const portalRef = useRef<ReactPortal>(null);
-  const ref = useCallback((el: HTMLTemplateElement) => {
+  const callbackRef = useCallback((el: HTMLTemplateElement) => {
     portalRef.current = createPortal(
       children,
       el.content as unknown as Element
     );
   }, []);
 
-  if (typeof window === "undefined") return <template {...props}>{children}</template>;
-  else return <template ref={ref} />;
-}
+  if (typeof window === "undefined")
+    return <template {...props}>{children}</template>;
+  else return <template ref={mergeRefs([callbackRef, ref])} />;
+});
